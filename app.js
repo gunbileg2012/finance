0e5b2bc86a8b0325f046b0fee5d18136f7322bd5
gunbileg <gunbileg92@gmail.com>
2020-09-06 11:37:23 +0800

    7

--- app.js
+++ app.js
@@ -5,18 +5,42 @@ var  uiController = (function(){
         inputDescription: ".add__description",
         inputValue: ".add__value",
         addBtn: ".add__btn",
+        total: ".budget__value",
+        precent: ".budget__expenses--percentage",
+        totalInc: ".budget__income--value",
+        totalExp: ".budget__expenses--value",
     }
     return {
         getInput: function(){
             return {
                 type: document.querySelector(DomStrings.inputType).value,
                 description: document.querySelector(DomStrings.inputDescription).value,
-                value: document.querySelector(DomStrings.inputValue).value,
+                value: parseInt(document.querySelector(DomStrings.inputValue).value),
             }
         },
 
         getDomStrings: function(){
             return DomStrings;
+        },
+
+        clearFields: function(){
+            var fields = document.querySelectorAll(DomStrings.inputDescription + "," + DomStrings.inputValue);
+            // Convert list to Array
+            var fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(el, index, array){
+                el.value = "";
+            });
+            fieldsArr[0].focus();
+            /*for(var i = 0; i < fieldsArr.length; i++){
+                fieldsArr[i].value = "";
+            }*/
+        },
+        
+        banalcePrint: function(dataBanalce){
+            document.querySelector(DomStrings.total).textContent = dataBanalce.total;
+            document.querySelector(DomStrings.precent).textContent = dataBanalce.precent+'%';
+            document.querySelector(DomStrings.totalInc).textContent = '+ '+dataBanalce.totalInc;
+            document.querySelector(DomStrings.totalExp).textContent = '- '+dataBanalce.totalExp;
         },
 
         addListItem: function(item, type){
@@ -29,6 +53,7 @@ var  uiController = (function(){
                 list = '.expenses__list';
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%desc%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
+
             // Тэр HTML дотроо зарлагийн утгуудыг REPLACE ашиглаж өөрчилж өгнө
             html = html.replace('%id%', item.id);
             html = html.replace('%desc%', item.description);
@@ -44,7 +69,15 @@ var  uiController = (function(){
 
 
 var  financeController = (function(){
-    
+
+        var calculateTotal = function(type){
+            var sum = 0;
+            data.items[type].forEach(function(el, index, array){
+                sum = sum + el.value;
+            });
+            return data.totals[type] = sum;
+        }
+
         var inCome = function(id, description, value){
             this.id = id;
             this.description = description;
@@ -68,7 +101,9 @@ var  financeController = (function(){
             totals: {
                 inc : 0,
                 exp : 0,
-            }
+            },
+            total: 0,
+            precent: 0,
         };
 
         return {
@@ -90,6 +125,22 @@ var  financeController = (function(){
 
             data: function(){
                 return data;
+            },
+
+            calculate: function(){
+                calculateTotal('inc');
+                calculateTotal('exp');
+                data.total = data.totals.inc - data.totals.exp;
+                data.precent = Math.round((data.totals.exp / data.totals.inc) * 100);
+            },
+
+            resultCalculate: function(){
+                return {
+                    total : data.total,
+                    precent: data.precent,
+                    totalInc : data.totals.inc,
+                    totalExp: data.totals.exp,
+                }
             }
         }
 })();
@@ -100,14 +151,23 @@ var  appController = (function(uiController,financeController){
        // 1. Оруулах өгөгдлийг дэлгэцээс олж авах
        // 2. Олж авсан өгөгдлүүдийн санхүүгийн контроллерт дамжуулж тэнд хадгална
        // 3. Олж авсан өгөгдлүүдийн вэб сайтын тохирох хэсэгт гаргана
-       // 4. Төсвийг тооцоолно
+       
        // 5. Эцсийн үлдэглийг тооцоог дэлгэцэнд харуулна
     var ctrlAddItem = function(){
       var input = uiController.getInput();
-      var item = financeController.addItem(input.type, input.description, input.value);
-     console.log(financeController.data());
-     uiController.addListItem(item, input.type);
-       
+
+      if(input.description !== "" && input.value !== ""){
+        var item = financeController.addItem(input.type, input.description, input.value);
+        uiController.addListItem(item, input.type);
+        uiController.clearFields();
+        // 4. Төсвийг тооцоолно
+        financeController.calculate();
+        // 5. Эцсийн үлдэглийг тооцоог дэлгэцэнд харуулна
+        var resultCalculate = financeController.resultCalculate();
+        // 6. Төсвийн дэлгэцэнд харуулах
+        uiController.banalcePrint(resultCalculate);
+      }
+
     };
 
     
@@ -121,7 +181,6 @@ var  appController = (function(uiController,financeController){
         document.querySelector(DOM.addBtn).addEventListener("keypress", function(event){   
            // 
            if(event.keyCode === 13){
-            console.log(event.keyCode);
             ctrlAddItem();
            }
         });
@@ -130,6 +189,12 @@ var  appController = (function(uiController,financeController){
     return {
         init: function(){
             console.log('Application started .......');
+            uiController.banalcePrint({
+                total : 0,
+                precent: 0,
+                totalInc : 0,
+                totalExp: 0,
+            });
             setupEventListeners();
         }
     }
